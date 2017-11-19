7ddb2ee6ac4b33afdc40691463b1acbe646c1848
Adam Berg <adam@xyzdigital.com>
2017-11-19 12:30:11 -0800

    Use Org controller

--- ccar-be.go
+++ ccar-be.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	controllers "github.com/adamjberg/supreme-telegram/controllers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	controllers.NewOrganizationController(router)
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
--- controllers/organization_controller.go
+++ controllers/organization_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -19,15 +20,19 @@ func NewOrganizationController(router mux.Router) *OrganizationController {
 	s.HandleFunc("", c.create).Methods("POST")
 	s.HandleFunc("/{id}", c.delete).Methods("DELETE")
 
+	fmt.Println("Org controller")
+
 	return c
 }
 
 func (c *OrganizationController) get(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Println("get")
+	w.Write([]byte("Success"))
 }
 
 func (c *OrganizationController) getAll(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Println("getAll")
+	w.Write([]byte("Success"))
 }
 
 func (c *OrganizationController) delete(w http.ResponseWriter, r *http.Request) {
