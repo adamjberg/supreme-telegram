bfc6d6a624a0c0fc37bf8997ce11a4264310952b
Adam Berg <adam@xyzdigital.com>
2017-11-19 13:07:43 -0800

    Add org controller routes

--- ccar-be.go
+++ ccar-be.go
@@ -6,10 +6,16 @@ import (
 
 	controllers "github.com/adamjberg/supreme-telegram/controllers"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
 )
 
 func main() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic(err)
+	}
+
 	router := mux.NewRouter()
-	controllers.NewOrganizationController(router)
+	controllers.NewOrganizationController(router, db)
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
--- controllers/organization_controller.go
+++ controllers/organization_controller.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"github.com/adamjberg/supreme-telegram/models"
-
 	"github.com/jinzhu/gorm"
 
 	"github.com/gorilla/mux"
@@ -30,30 +33,74 @@ func NewOrganizationController(router *mux.Router, db *gorm.DB) *OrganizationCon
 	return c
 }
 
-func (c *OrganizationController) GetTable() *gorm.DB {
-	return c.table
-}
-
-func (c *OrganizationController) GetModel() models.Organization {
-	return models.Organization{}
-}
-
-func (c *OrganizationController) GetModels() []models.Organization {
-	return []models.Organization{}
-}
-
 func (c *OrganizationController) get(w http.ResponseWriter, r *http.Request) {
-
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	model := models.Organization{
+		ID: uint(id),
+	}
+	c.table.First(&model, model)
+	res, err := json.Marshal(model)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Write(res)
 }
 
 func (c *OrganizationController) getAll(w http.ResponseWriter, r *http.Request) {
-	CrudGetAll(c, w)
+	var models []models.Organization
+	c.table.Find(&models)
+	res, err := json.Marshal(models)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.Write(res)
 }
 
 func (c *OrganizationController) delete(w http.ResponseWriter, r *http.Request) {
-
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	model := models.Organization{
+		ID: uint(id),
+	}
+	delete := c.table.Delete(&model)
+	if delete.Error != nil {
+		fmt.Println(delete.Error)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func (c *OrganizationController) create(w http.ResponseWriter, r *http.Request) {
-
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var model models.Organization
+	err = json.Unmarshal(body, &model)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	create := c.table.Create(&model)
+	if create.Error != nil {
+		fmt.Println(create.Error)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusAccepted)
 }
